c391a830aa9e73de73bcbddfed3090d9ee171da5
Tech-a44 <tech-a44@Tech-A44s-Air.lan>
2019-05-07 22:49:20 +0100

    got basic calculator working

--- app.rb
+++ app.rb
@@ -3,12 +3,32 @@ puts "choose calculator (b)asic or (a)dvance"
 choice = gets.chomp.to_s
 
 if choice == "b"
+  print "choose an operator"
+
+  op = gets.chomp()
+
   print "choice a number"
+
   num1 = gets.chomp().to_f
+
   print "choice another number"
+
   num2 = gets.chomp().to_f
+
+  if op == "+"
+    puts (num1 + num2)
+  elsif op == "-"
+    puts (num1 - num2)
+  elsif op == "*"
+    puts (num1 * num2)
+  elsif op == "/"
+    puts (num1 / num2)
+  else
+    puts "invalid operator"
+  end
+  
 elsif choice == "a"
   print "advance"
 else
-  print "invalid operator"
+  print "invalid input"
 end
